87970fd199d5f0315cc185b2783d5b6d30fa6c38
tbelier <titouan.belier@gmail.com>
2024-07-21 20:56:58 +0200

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,26 @@
+const map = L.map('map').fitWorld();
+
+	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+
+function onLocationFound(e) {
+    const radius = e.accuracy / 2;
+    
+    const locationMarker = L.marker(e.latlng).addTo(map)
+        .bindPopup(`You are within ${radius} meters from this point<br>
+                    Your latitude is : ${e.latlng.lat.toFixed(2)}<br>
+                    Your longitude is : ${e.latlng.lng.toFixed(2)}`).openPopup();
+
+    const locationCircle = L.circle(e.latlng, radius).addTo(map);
+}
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationfound', onLocationFound);
+map.on('locationerror', onLocationError);
+
+map.locate({setView: true, maxZoom: 16});
